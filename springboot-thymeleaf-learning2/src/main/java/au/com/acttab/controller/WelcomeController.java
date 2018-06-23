ff685d729cf9f17d47ad0ecb54937cd46fb0708c
Jatandar <Jatandar@Jatandar-PC>
2018-06-23 14:54:43 +1000

    Embedded database initial commit

---
+++ springboot-thymeleaf-learning2/src/main/java/au/com/acttab/controller/WelcomeController.java
@@ -0,0 +1,27 @@
+package au.com.acttab.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	String chapters[]= new String[]{"Quick Start With Groovy","Quick Start With Java",
+            "Debugging and Managing Your App",
+            "Data Access with SpringBoot",
+            "Securing Your App"};
+	
+	@RequestMapping("/welcome")
+    public String loginMessage(Model m){
+		m.addAttribute("name", "Jatandar Dhirwani");
+		m.addAttribute("chapters", chapters);
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-YYYY");
+		System.out.println("simpleDateFormat.format()=" + simpleDateFormat.format(new Date()));
+		m.addAttribute("toDoDate", simpleDateFormat.format(new Date()));
+        return "welcome";
+    }
+}
